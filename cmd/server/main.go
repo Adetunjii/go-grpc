ebfcaf72d0aa88a85c344295a7b942425e01b329
adetunjii <adetunjithomas1@outlook.com>
2022-07-26 23:31:41 +0100

    feat: grpc interceptor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"github.com/Adetunjii/go-grpc/pb"
@@ -8,18 +9,67 @@ import (
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"time"
 )
+
+func unaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	log.Println("--> unary interceptor: ", info.FullMethod)
+	return handler(ctx, req)
+}
+
+const (
+	secretKey     = "secret"
+	tokenDuration = 15 * time.Minute
+)
+
+func accessibleRoles() map[string][]string {
+	const laptopServicePath = "LaptopService/"
+	return map[string][]string{
+		laptopServicePath + "CreateLaptop": {"admin"},
+		laptopServicePath + "UploadImage":  {"user"},
+	}
+}
+
+func seedUsers(userStore service.UserStore) error {
+	err := createUser(userStore, "admin1", "secret", "admin")
+	if err != nil {
+		return err
+	}
+
+	return createUser(userStore, "user1", "secret", "user")
+}
+
+func createUser(userStore service.UserStore, username, password, role string) error {
+	user, err := service.NewUser(username, password, role)
+	if err != nil {
+		return err
+	}
+	return userStore.Save(user)
+}
 
 func main() {
 	port := flag.Int("port", 0, "server port")
 	flag.Parse()
 	log.Printf("start server on port %d", *port)
 
+	userStore := service.NewInMemoryUserStore()
+	err := seedUsers(userStore)
+	if err != nil {
+		log.Fatal("cannot seed users")
+	}
+	jwtManager := service.NewJWTManager(secretKey, tokenDuration)
+	authServer := service.NewAuthServer(userStore, jwtManager)
+
 	laptopStore := service.NewInMemoryLaptopStore()
 	imageStore := service.NewDiskImageStore("img")
 
 	laptopServer := service.NewLaptopServer(laptopStore, imageStore)
-	grpcServer := grpc.NewServer()
+
+	interceptor := service.NewAuthInterceptor(jwtManager, accessibleRoles())
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(interceptor.Unary()))
+
+	pb.RegisterAuthServiceServer(grpcServer, authServer)
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
 
 	address := fmt.Sprintf("0.0.0.0:%d", *port)
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/Adetunjii/go-grpc
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.3.0
 	github.com/jinzhu/copier v0.3.5
 	github.com/stretchr/testify v1.7.1
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
 	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.28.0
 )
